49b040d9fa415284f8f1a3a861c1c5ca6986f589
Dreamzzwq <ibrahimrizky696@gmail.com>
2026-02-10 14:59:59 +0700

    menambah file error404419

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404-not found</title>
+    <style>
+        body { background-color: #ffffff; color: black; font-family: Arial, sans-serif; text-align: center; padding-top: 150px; }
+        h1 { font-size: 5rem; margin: 140px; }
+        p { font-size: 1.2rem; margin: 20px 0; }
+        a { color: #000000; text-decoration: none; font-weight: bold; }
+    </style>
+</head>
+<body>
+    <h1>404 | NOT FOUND</h1>
+    <a href="/">Kembali ke Beranda</a>
+</body>
+</html>   
