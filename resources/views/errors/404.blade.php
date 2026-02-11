bbfbdec704c70de2050dbb74561436624e2a03cf
Dreamzzwq <ibrahimrizky696@gmail.com>
2026-02-11 10:15:58 +0700

    menambahkan fitur ip adres di error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,12 +7,19 @@
     <style>
         body { background-color: #ffffff; color: black; font-family: Arial, sans-serif; text-align: center; padding-top: 150px; }
         h1 { font-size: 5rem; margin: 140px; }
+        h5 { font-size: 4;}
         p { font-size: 1.2rem; margin: 20px 0; }          
         a { color: #000000; text-decoration: none; font-weight: bold; }
     </style>
 </head>
 <body>
     <h1>404 | NOT FOUND</h1>
+    <h5>You IP ADDRESS :</h5>
+            <?php
+                $ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
+                print_r($ip);
+                die()
+            ?>
     <a href="/">Kembali ke Beranda</a>
 </body>
 </html>   
---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404-not found</title>
+    <style>
+        body { background-color: #ffffff; color: black; font-family: Arial, sans-serif; text-align: center; padding-top: 150px; }
+        h1 { font-size: 5rem; margin: 140px; }
+        h5 { font-size: 4;}
+        p { font-size: 1.2rem; margin: 20px 0; }          
+        a { color: #000000; text-decoration: none; font-weight: bold; }
+    </style>
+</head>
+<body>
+    <h1>419 | SESSION EXPIRED</h1>
+    <h5>You IP ADDRESS</h5>
+        <?php
+            $ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
+            die()
+        ?>
+    <a href="/">Kembali ke Beranda</a>
+</body>
+</html>   
